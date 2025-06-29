d872cf1db97a9e3ac7550601dc4273a2afbf0ce7
VolodymyrOstrikov <75792340+VolodymyrOstrikov@users.noreply.github.com>
2025-06-29 16:19:19 +0200

    Completed homework

--- src/CastleAdventure.java
+++ src/CastleAdventure.java
@@ -5,12 +5,9 @@ public class CastleAdventure {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Нижче перша частина завдання для прикладу
-        // Там де в завданні вказані початкові дані у вигляді масиву - ініціалізуйте окремий масив
-        // Нижче обробляйте масив, додайте логіку обробки
-        // Частина 1: Увійди в Замок
+//Part 1: Enter the Castle
         int[] secretCode = {7, 14, 28, 56, 112};
-        System.out.println("Введіть 5 чисел, щоб відкрити двері замку:");
+        System.out.println("Enter the 5 secret numbers to unlock the castle:");
         boolean isCodeCorrect = true;
 
         for (int i = 0; i < secretCode.length; i++) {
@@ -21,11 +18,80 @@ public class CastleAdventure {
         }
 
         if (isCodeCorrect) {
-            System.out.println("Замок відкрито! Заходьте всередину.");
+            System.out.println("the door lock is open... Welcome to the Castle!");
         } else {
-            System.out.println("Невірний код. Спробуйте ще.");
+            System.out.println("Wrong code! The door stays closed.");
         }
 
-        // Інші частини пригоди додайте тут!
+//Part 2: Solve the Magic Board
+        int[] magicBoard = {3, 6, 9, 12, 15, 18, 21, 24, 27, 30};
+        int sum = 0;
+
+        for (int num : magicBoard) {
+            sum += num;
+        }
+
+        System.out.println("\nThe sum of the magic numbers is: " + sum);
+        System.out.println("\nThe next room is open");
+
+        //Part 3: Defeat the Iron Dragon
+        int[] dragonAttacks = {8, 16, 24, 32, 40};
+        boolean allEven = true;
+
+        for (int attack : dragonAttacks) {
+            if (attack % 2 != 0) {
+                allEven = false;
+                break;
+            }
+        }
+
+        if (allEven) {
+            System.out.println("\nAll attacks are even numbers! Shield activated!");
+        } else {
+            System.out.println("\nWarning! Odd attack!");
+        }
+
+        // Part 4: Find the Magic Key
+        System.out.print("\nWhat is your favorite number? ");
+        int favoriteNumber = scanner.nextInt();
+        boolean found = false;
+
+        // Verification in secretCode
+        for (int num : secretCode) {
+            if (num == favoriteNumber) {
+                found = true;
+                break;
+            }
+        }
+
+        // If not found, verification in magicBoard
+        if (!found) {
+            for (int num : magicBoard) {
+                if (num == favoriteNumber) {
+                    found = true;
+                    break;
+                }
+            }
+        }
+
+        // If not found, verification in dragonAttacks
+        if (!found) {
+            for (int num : dragonAttacks) {
+                if (num == favoriteNumber) {
+                    found = true;
+                    break;
+                }
+            }
+        }
+
+        if (found) {
+            System.out.println("Your key has been found! Magic is on your side!");
+        } else {
+            System.out.println("You need more magic...");
+        }
+
+        scanner.close();
     }
-}
+    }
+
+
